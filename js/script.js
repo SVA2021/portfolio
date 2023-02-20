2ecb44009b2d4fb9d5ce2e4013c93657d973ce34
SVA2021 <sofronov.vit@gmail.com>
2023-02-20 16:51:40 +0400

    update status of projects

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const timerWrapper = document.getElementById('timer');
-let defaultTime = 42; // answer for all questions in the universe
+let defaultTime = 63; // add more time after every release
 
 const timer = setInterval(() => {
-  --defaultTime;
-  timerWrapper.innerHTML = defaultTime;
-  if (defaultTime == 0) {
-    clearInterval(timer);
-    location.href = 'https://sva2021.github.io/MyPortfolio';
-  }
+    --defaultTime;
+    timerWrapper.innerHTML = defaultTime;
+    if (defaultTime == 0) {
+        clearInterval(timer);
+        location.href = 'https://sva2021.github.io/MyPortfolio';
+    }
 }, 1000);
